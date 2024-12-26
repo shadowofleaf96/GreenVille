d7ea8b5bc0ab1a71a9ba60c74d8e89d892a31124
Shadow Of Leaf <simokb46@gmail.com>
2024-12-26 20:10:53 +0100

    feat: initial commit for android and ios app using capacitor

---
+++ client/android/app/src/main/java/ma/mk/greenville/MainActivity.java
@@ -0,0 +1,5 @@
+package ma.mk.greenville;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ma.mk.greenville',
+  appName: 'GreenVille',
+  webDir: 'dist'
+};
+
+export default config;
