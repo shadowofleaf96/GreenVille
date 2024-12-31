eae0457424ab18ea7abd4f588e883b05470f0044
Shadow Of Leaf <simokb46@gmail.com>
2024-12-31 12:28:35 +0100

    fix: scroll to next product in top deals is working now

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -330,7 +330,7 @@ const forgotPassword = async (req, res) => {
       subject: "Password Reset",
       text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
         Please click on the following link, or paste this into your browser to complete the process:\n\n
-        ${process.env.URL}/admin/reset-password/${resetToken}\n\n
+        ${process.env.FRONTEND_URL}/admin/reset-password/${resetToken}\n\n
         If you did not request this, please ignore this email and your password will remain unchanged.\n`,
     };
 
