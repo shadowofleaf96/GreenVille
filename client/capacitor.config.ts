dae166d8557f6fdd941a0abee748cb0b9bafd8f9
Shadow Of Leaf <simokb46@gmail.com>
2025-01-20 18:47:36 +0100

    fix: now the backend working

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -1,9 +1,13 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'ma.mk.greenville',
-  appName: 'GreenVille',
-  webDir: 'dist'
+  appId: "ma.mk.greenville",
+  appName: "GreenVille",
+  webDir: "dist",
+  server: {
+    url: "https://greenville-frontend.onrender.com",
+    cleartext: true
+  },
 };
 
 export default config;
