085898c1361db6d49c671644768dc09d4b845c54
Shadow Of Leaf <shadowofleaf96@gmail.com>
2025-01-30 19:30:54 +0100

    fix: use AndroidInterface

--- client/src/frontoffice/components/profileLinks/ProfileLink.jsx
+++ client/src/frontoffice/components/profileLinks/ProfileLink.jsx
@@ -23,7 +23,9 @@ const ProfileLink = () => {
       localStorage.removeItem("customer_access_token");
       localStorage.removeItem("isAuthenticated");
       dispatch(logout());
-      Android.onLogout();
+      if (window.AndroidInterface) {
+        window.AndroidInterface.onLogout();
+      }
       toast.success(t("You have been Logged out"));
       router("/");
     } catch (error) {
