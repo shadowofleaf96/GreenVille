1d57f9ce3d3ba89c15a3ea28f04dde1d6151f8ab
Shadow Of Leaf <simokb46@gmail.com>
2026-01-16 16:41:59 +0100

    fix: restore product-table-head

---
+++ client/src/backoffice/sections/product/product-table-head.jsx
@@ -0,0 +1,91 @@
+import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
+
+import { TableHead, TableRow, TableHeader } from "@/components/ui/table";
+import { Checkbox } from "@/components/ui/checkbox";
+import Iconify from "../../../components/iconify";
+
+export default function ProductTableHead({
+  order,
+  orderBy,
+  rowCount,
+  headLabel,
+  numSelected,
+  onRequestSort,
+  onSelectAllClick,
+}) {
+  const { t } = useTranslation();
+
+  const onSort = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+    <TableHeader className="bg-gray-50/50 border-y border-gray-100 font-bold">
+      <TableRow className="hover:bg-transparent">
+        <TableHead className="p-4 w-12 text-center">
+          <Checkbox
+            checked={rowCount > 0 && numSelected === rowCount}
+            onCheckedChange={(checked) => {
+              onSelectAllClick({
+                target: { checked },
+              });
+            }}
+          />
+        </TableHead>
+
+        {headLabel.map((headCell) => (
+          <TableHead
+            key={headCell.id}
+            className="p-4 font-bold text-gray-600 uppercase tracking-wider text-xs select-none"
+            onClick={onSort(headCell.id)}
+            style={{
+              width: headCell.width,
+              minWidth: headCell.minWidth,
+            }}
+          >
+            <div className="flex items-center gap-1 hover:text-primary transition-colors cursor-pointer group">
+              <span>{t(headCell.label)}</span>
+              {headCell.id !== "" && headCell.id !== " " && (
+                <div className="flex flex-col">
+                  {orderBy === headCell.id ? (
+                    <Iconify
+                      icon={
+                        order === "asc"
+                          ? "material-symbols:arrow-drop-up-rounded"
+                          : "material-symbols:arrow-drop-down-rounded"
+                      }
+                      width={20}
+                      className="text-primary"
+                    />
+                  ) : (
+                    <Iconify
+                      icon="material-symbols:unfold-more-rounded"
+                      width={20}
+                      className="text-gray-400 opacity-0 group-hover:opacity-100 transition-opacity"
+                    />
+                  )}
+                </div>
+              )}
+            </div>
+            {orderBy === headCell.id && (
+              <span className="sr-only">
+                {order === "desc" ? "sorted descending" : "sorted ascending"}
+              </span>
+            )}
+          </TableHead>
+        ))}
+      </TableRow>
+    </TableHeader>
+  );
+}
+
+ProductTableHead.propTypes = {
+  order: PropTypes.oneOf(["asc", "desc"]),
+  orderBy: PropTypes.string,
+  rowCount: PropTypes.number,
+  headLabel: PropTypes.array,
+  numSelected: PropTypes.number,
+  onRequestSort: PropTypes.func,
+  onSelectAllClick: PropTypes.func,
+};
