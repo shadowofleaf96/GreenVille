92cacc63f34f2458baf89a062345f3145cd2c31b
Shadow Of Leaf <simokb46@gmail.com>
2026-02-11 16:41:45 +0100

    fix: fix vercel build

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -29,8 +29,8 @@ const nextConfig = {
       },
     ],
   },
-  output: "export", // Outputs a Single-Page Application (SPA).
-  distDir: "./dist", // Changes the build output directory to `./dist/`.
+  output: "standalone",
+  distDir: "./dist",
   reactStrictMode: true,
 };
 
